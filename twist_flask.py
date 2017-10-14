c8324f60ea635cb751079f4794ebc1046c2ba822
Rallade <rallade@hotmail.com>
2017-10-15 00:03:04 +0100

    multiplicity
    added multiple results and cleaned it up

--- twist_flask.py
+++ twist_flask.py
@@ -1,9 +1,26 @@
 from flask import Flask, jsonify, request
 from skyscanner_cred import *
 import requests
+import time
 app = Flask(__name__)
 
 api = 'http://partners.api.skyscanner.net/apiservices/'
+
+data = {
+"cabinclass":"Economy",
+"country":"UK",
+"currency":"EUR",
+"locale":"en-GB",
+"locationSchema":"iata",
+"originplace":"MAD",
+"destinationplace":"LON",
+"outbounddate":"2018-05-30",
+"inbounddate":"2018-06-02",
+"adults":"1",
+"children":"1",
+"infants":"0",
+"apikey": flights_and_car
+}
 
 @app.route('/test', methods=['POST'])
 def test():
@@ -19,44 +36,52 @@ def test():
         arguments = argument.split()
         command = request.form['command']
         origin = get_airport(arguments[1])
-        destination = get_airport(arguments[0])
-        (journey, carriers) = get_journey(origin, destination)
+        data['originplace'] = origin['PlaceId'][:3]
+        data['country'] = origin['CountryId'][:2]
+        destination = get_airport(arguments[0][:3])
+        (journeys, carriers) = get_journey(origin, destination)
         try:
-            price = journey['MinPrice']
+            journeys[0]
         except IndexError:
-            return jsonify({'content' : 'There was an error with the wording of zour request, please try again'})
-        departure = journey['OutboundLeg']['DepartureDate']
-        arrival = journey['InboundLeg']['DepartureDate']
-
-        carrier_out_names = []
-        carrier_in_names = []
-
-        for cid in journey['OutboundLeg']['CarrierIds']:
-            for carrier in carriers:
-                if cid == carrier['CarrierId']:
-                    carrier_out_names.append(carrier['Name'])
-        print(carrier_out_names)
-        
-
-        for cid in journey['InboundLeg']['CarrierIds']:
-            for carrier in carriers:
-                if cid == carrier['CarrierId']:
-                    carrier_in_names.append(carrier['Name'])
-        print(carrier_in_names)
+            return jsonify({'content' : 'There was an error with the wording of your request, please try again with a different input'})
 
         response = ('From ' + origin['PlaceName'] + ' to ' + destination ['PlaceName'])
-        response += '\nThe journey will cost you about '
-        response += str(price)
-        response += ' euros.\nDeparture: '
-        response += departure[:10]
-        response += ' and will be with '
-        for carrier in carrier_out_names:
-            response += carrier
-        response += '\nArrival: '
-        response += arrival[:10]
-        response += ' and will be with '
-        for carrier in carrier_in_names:
-            response += carrier
+
+        for journey in journeys[:3]:
+
+            price = journey['MinPrice']
+            departure = journey['OutboundLeg']['DepartureDate']
+            arrival = journey['InboundLeg']['DepartureDate']
+
+            carrier_out_names = []
+            carrier_in_names = []
+
+            for cid in journey['OutboundLeg']['CarrierIds']:
+                for carrier in carriers:
+                    if cid == carrier['CarrierId']:
+                        carrier_out_names.append(carrier['Name'])
+            print(carrier_out_names)
+            
+
+            for cid in journey['InboundLeg']['CarrierIds']:
+                for carrier in carriers:
+                    if cid == carrier['CarrierId']:
+                        carrier_in_names.append(carrier['Name'])
+            print(carrier_in_names)
+
+
+            response += '\n* This journey will cost you about '
+            response += ('**'+str(price)+'**')
+            response += ' euros.\nDeparture: '
+            response += departure[:10]
+            response += ' and will be with '
+            for carrier in carrier_out_names:
+                response += carrier
+            response += '\nArrival: '
+            response += arrival[:10]
+            response += ' and will be with '
+            for carrier in carrier_in_names:
+                response += carrier
 
         return jsonify({'content': response})       
         
